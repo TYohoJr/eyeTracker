dd494632ff44b9861d6d452a7f29021781915385
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-01 11:21:06 -0600

    added todo list

---
+++ src/ToDo.js
@@ -0,0 +1,13 @@
+// Add home page with multiple exercise options
+
+// Exercises to add:
+// ☐ Static dots
+// ☐ Pursuits (Scrolling dots)
+// ☐ Saccades (Frame jumping dots)
+// ☐ Combination saccades/pursuits (Scrolling and jumping dots)
+// ☐ Random saccades (Random dots)
+// ☐ Antisaccades (Static center/flashing side dots)
+// ☐ OPK (Scrollng bars)
+// ☐ Hemistim (Flashing checkerboard)
+// ☐ Memory saccades(maybe...)
+// ☐ Gaze stability (Need further research)
