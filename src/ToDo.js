0af1e79b7fcd6ad24a13601fe1197665b04fb0d0
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-10 09:26:53 -0500

    updated to-do list

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,15 +1,17 @@
-// Add home page with multiple exercise options
+// To Do
 
-// Split reducers into individual exercises
-
-// Exercises to add:
-// ️️☑️ Static dots
-// ☑️ Pursuits (Scrolling dots)
-// □ Saccades (Frame jumping dots)
-// □ Combination saccades/pursuits (Scrolling and jumping dots)
-// ☑️ Random saccades (Random dots)
-// □ Antisaccades (Static center/flashing side dots)
-// ☑️ OPK (Scrollng bars)
-// □ Hemistim (Flashing checkerboard)
-// □ Memory saccades(maybe...)
-// □ Gaze stability (Need further research)
+// Move static CSS properties to CSS files
+// Make sure all classes/id's are unique to the exercise to avoid accidental CSS
+// Try doing combinations using instances of a class to see if keyframes will start on mount
+// Add more dot color options (green, yellow, orange, red, purple, blue, black)
+// Conditional options for pursuits in combinations exercise
+// Redesign all buttons
+// Re-do all speed options to make a smoother gap between options
+// Separate the done button div and the rest of the exercise page
+// Try to give pursuits a cycles option
+// Make saccades last dot less jumpy
+// Make cycles of random saccades a drop down list (also add infinite option)
+// Change homepage images to percent sizes instead of static (unless that ends up breaking it on small screens...)
+// Integrate a database so someone can save their exercise settings and load them on login (noSQL would probably make the most sense)
+// Add navbar for login option
+// Add footer to homepage
