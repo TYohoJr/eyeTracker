e44f6b67a4f7f4591dbed06466eda964f81d788c
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-11 15:33:30 -0500

    updated todo list

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,7 +1,13 @@
 // To Do
 
 // Try doing combinations using instances of a class to see if keyframes will start on mount(ok this is harder that i thought it would be...)
-// Redesign all buttons
 // Re-do all speed options to make a smoother gap between options
-// Integrate a database so someone can save their exercise settings and load them on login (noSQL would probably make the most sense)
-// Add navbar for login option
+// Functional log in
+// Functional log out
+// jsonwebtokens implemented
+// Home button in navabar
+// Remove reset buttons
+// Add save exercise settings button (somewhere...)
+// Integrate exercise settings into database
+// Load saved exercises into reducer on log in
+// Conditionally pull saved exercises on options pages mounting
